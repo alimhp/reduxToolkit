dcd417ed3ecad3eec2354859d0338e22e14306dc
alimhp <alimohamadipourr@gmail.com>
2022-09-09 23:12:40 +0430

    toggle and delete todos

--- src/component/TodoItem.js
+++ src/component/TodoItem.js
@@ -1,14 +1,27 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { toggleTodos, deletedTodos } from "../featurs/Todo/TodoSlice";
 
 const TodoItem = ({ id, title, completed }) => {
+  const dispatch = useDispatch();
   return (
     <li className={`list-group-item ${completed && "list-group-item-success"}`}>
       <div className="flex justify-between">
         <span className="d-flex align-items-center">
-          <input type="checkbox" className="mr-3" checked={completed}></input>
+          <input
+            type="checkbox"
+            className="mr-3"
+            checked={completed}
+            onChange={(e) => dispatch(toggleTodos({ id }))}
+          ></input>
           {title}
         </span>
-        <button className="btn btn-danger">Delete</button>
+        <button
+          className="btn btn-danger"
+          onClick={() => dispatch(deletedTodos({ id }))}
+        >
+          Delete
+        </button>
       </div>
     </li>
   );
--- src/featurs/Todo/TodoSlice.js
+++ src/featurs/Todo/TodoSlice.js
@@ -1,13 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = [
-  { id: 1, title: "todo1", completed: false },
-  { id: 2, title: "todo2", completed: false },
-  { id: 3, title: "todo3", completed: true },
-  { id: 4, title: "todo4", completed: false },
-  { id: 5, title: "todo5", completed: true },
-];
-
+const initialState = {
+  todos: [
+    { id: 1, title: "todo1", completed: false },
+    { id: 2, title: "todo2", completed: false },
+    { id: 3, title: "todo3", completed: true },
+    { id: 4, title: "todo4", completed: false },
+    { id: 5, title: "todo5", completed: true },
+  ],
+};
 const todoSlice = createSlice({
   name: "todos",
   initialState,
@@ -19,10 +20,18 @@ const todoSlice = createSlice({
         title: action.payload.title,
         completed: false,
       };
-      state.push(newTodo);
+      state.todos.push(newTodo);
+    },
+    toggleTodos: (state, action) => {
+      const selectedTodos = state.todos.find((t) => t.id === action.payload.id);
+      selectedTodos.completed = !selectedTodos.completed;
+    },
+    deletedTodos: (state, action) => {
+      const filterTodos = state.todos.filter((t) => t.id !== action.payload.id);
+      state.todos = filterTodos;
     },
   },
 });
 // Action creators are generated for each case reducer function
-export const { addTodo } = todoSlice.actions;
+export const { addTodo, toggleTodos, deletedTodos } = todoSlice.actions;
 export default todoSlice.reducer;
